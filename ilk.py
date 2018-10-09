07a81defb7c0443a06dd7229ce8c371894c29ea4
Gokberk Serin <gokberkserin@gmail.com>
2018-10-09 18:01:07 +0300

    third commit

--- ilk.py
+++ ilk.py
@@ -1,11 +1,18 @@
-print("zxc")
+from stl import mesh
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
 
-degisken = "asd"
-degisken = 1
+# Create a new plot
+figure = pyplot.figure()
+axes = mplot3d.Axes3D(figure)
 
-print(int(degisken) + int(2))
+# Load the STL files and add the vectors to the plot
+your_mesh = mesh.Mesh.from_file('tests/stl_binary/HalfDonut.stl')
+axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
 
-for i in range(0,10,3):
-    print(i)
+# Auto scale to the mesh size
+scale = your_mesh.points.flatten(-1)
+axes.auto_scale_xyz(scale, scale, scale)
 
-print("gokberk")
+# Show the plot to the screen
+pyplot.show()
